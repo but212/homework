efcc13b2d9a4e0db901c3780fcb5268457e338e9
but212 <wjddlf211@gmail.com>
2025-05-18 01:27:42 +0900

    biome으로 병합합

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -5,32 +5,60 @@ module.exports = {
   semi: true,
   singleQuote: true,
   quoteProps: 'as-needed',
-  jsxSingleQuote: true,
   trailingComma: 'es5',
   bracketSpacing: true,
   bracketSameLine: false,
   arrowParens: 'always',
+  endOfLine: 'lf',
+
   htmlWhitespaceSensitivity: 'css',
   proseWrap: 'preserve',
-  endOfLine: 'lf',
+  singleAttributePerLine: false,
+  jsxSingleQuote: true,
+  jsxBracketSameLine: false,
+
   overrides: [
     {
       files: '*.md',
       options: {
-        printWidth: 120,
-        proseWrap: 'never',
+        printWidth: 100,
+        proseWrap: 'always',
+        singleQuote: true,
+        tabWidth: 2,
       },
     },
+
     {
       files: ['*.json', '*.jsonc'],
       options: {
         singleQuote: false,
+        trailingComma: 'none',
+        parser: 'json',
       },
     },
+
     {
       files: ['*.css', '*.scss', '*.less'],
       options: {
         singleQuote: false,
+        printWidth: 100,
+        tabWidth: 2,
+      },
+    },
+
+    {
+      files: '*.html',
+      options: {
+        printWidth: 100,
+        htmlWhitespaceSensitivity: 'css',
+      },
+    },
+
+    {
+      files: ['*.yaml', '*.yml'],
+      options: {
+        singleQuote: true,
+        tabWidth: 2,
       },
     },
   ],
